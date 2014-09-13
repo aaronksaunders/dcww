103f13328647ea258ea8f14e50620d0bb4cc3e10
Aaron Saunders <aaron@clearlyinnovative.com>
2014-09-12 20:43:40 -0400

    saving image after photo is taken

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,41 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
-})
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
+    .controller('FriendsCtrl', function ($scope, Friends, $cordovaCamera) {
+        $scope.friends = Friends.all();
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
+        // add function to take a picture using the camera
+        $scope.doTakePicture = function () {
+            var options = {
+                quality: 75,
+                destinationType: Camera.DestinationType.DATA_URL,
+                sourceType: Camera.PictureSourceType.CAMERA,
+                allowEdit: true,
+                encodingType: Camera.EncodingType.JPEG,
+                targetWidth: 200,
+                targetHeight: 200,
+                popoverOptions: CameraPopoverOptions,
+                saveToPhotoAlbum: false
+            };
 
-.controller('AccountCtrl', function($scope) {
-});
+            $cordovaCamera.getPicture(options).then(function (imageData) {
+                // Success! Image data is here
+                console.log("Success! Image data is here");
+                console.log(JSON.stringify(imageData));
+
+                Friends.save(imageData);
+
+                $scope.friends = Friends.all();
+            }, function (err) {
+                // An error occured. Show a message to the user
+            });
+        }
+    })
+
+    .controller('FriendDetailCtrl', function ($scope, $stateParams, Friends) {
+        $scope.friend = Friends.get($stateParams.friendId);
+        console.log(JSON.stringify($scope.friend));
+    })
+
+    .controller('AccountCtrl', function ($scope) {
+    });
--- www/js/services.js
+++ www/js/services.js
@@ -3,24 +3,32 @@ angular.module('starter.services', [])
 /**
  * A simple example service that returns some data.
  */
-.factory('Friends', function() {
-  // Might use a resource here that returns a JSON array
+    .factory('Friends', function () {
+        // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var friends = [
-    { id: 0, name: 'Scruff McGruff' },
-    { id: 1, name: 'G.I. Joe' },
-    { id: 2, name: 'Miss Frizzle' },
-    { id: 3, name: 'Ash Ketchum' }
-  ];
+        // Some fake testing data
+        var friends = [
+            { id: 0, name: 'Scruff McGruff' },
+            { id: 1, name: 'G.I. Joe' },
+            { id: 2, name: 'Miss Frizzle' },
+            { id: 3, name: 'Ash Ketchum' }
+        ];
 
-  return {
-    all: function() {
-      return friends;
-    },
-    get: function(friendId) {
-      // Simple index lookup
-      return friends[friendId];
-    }
-  }
-});
+        return {
+            all: function () {
+                return friends;
+            },
+            get: function (friendId) {
+                // Simple index lookup
+                return friends[friendId];
+            },
+            save: function (_data) {
+                var id = friends.length;
+                friends.push({
+                    id: id,
+                    name : new Date(),
+                    data : 'data:image/png;base64,'+_data
+                })
+            }
+        }
+    });
